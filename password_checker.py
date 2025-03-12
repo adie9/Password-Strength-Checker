4d89b16d1ddacc7a7df4dfb5d4312cfabf53aeb6
Adrian Guerra <70285960+adie9@users.noreply.github.com>
2025-03-12 16:40:55 -0400

    Added score calculator

--- password_checker.py
+++ password_checker.py
@@ -10,22 +10,39 @@ def password_checker(password):
         length = True
     else: length = False
 
-    # Does the password have numbers?
-    numbers = any(char.isdigit() for char in password)
-        
-
     # Does the password have letters?
     letters = any(char.isalpha() for char in password)
-
+    
+    # Does the password have numbers?
+    numbers = any(char.isdigit() for char in password)
+    
     # Does the password have symbols?
     symbols = special_character_checker(password)
-            
-    print(length)
-    print(numbers)
-    print(letters)
-    print(symbols)
 
-    # Scoring System where 0-2 = "Weak", 3 = "Moderate", and 4 = "Strong"
+    # Assign categories to list
+    category_list = [length, letters, numbers, symbols]
+
+    password_strength = score_calculator(category_list)
+    print(f"Your password strength is: {password_strength}")
+            
+    
+# Scoring System where 0-2 = "Weak", 3 = "Moderate", and 4 = "Strong"
+def score_calculator(categories):
+    score = 0
+
+    for cat in categories:
+        if cat == True:
+            score += 1
+    
+    # Calculate score
+    if score < 3:
+        return "Weak"
+    elif score == 3:
+        return "Moderate"
+    elif score == 4:
+        return "Strong"
+    
+    
 
 # Special Character checker function
 def special_character_checker(password):
