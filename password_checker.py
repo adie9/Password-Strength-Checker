c7726d8c57f264177cb7dbd18218b57f0dd5983b
Adrian Guerra <70285960+adie9@users.noreply.github.com>
2025-03-12 16:20:41 -0400

    Added a special character checker

--- password_checker.py
+++ password_checker.py
@@ -1,15 +1,14 @@
-# Main password strength checker function
-
+# Main Password Strength checker function
 def password_checker(password):
     length = False
     letters = False
     numbers = False
+    symbols = False
 
     # Is the password longer than eight characters?
     if len(password) > 8:
         length = True
     else: length = False
-
 
     # Does the password have numbers?
     numbers = any(char.isdigit() for char in password)
@@ -17,13 +16,26 @@ def password_checker(password):
 
     # Does the password have letters?
     letters = any(char.isalpha() for char in password)
+
+    # Does the password have symbols?
+    symbols = special_character_checker(password)
             
     print(length)
     print(numbers)
     print(letters)
+    print(symbols)
 
     # Scoring System where 0-2 = "Weak", 3 = "Moderate", and 4 = "Strong"
 
+# Special Character checker function
+def special_character_checker(password):
+        special_characters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')',
+        '-', '_', '+', '=', '[', ']', '{', '}', ';', ':', ',', '.', '<', '>',
+        '/', '?', '\\', '|', '`', '~']
+        
+        if any(char in special_characters for char in password):
+            return True
+        else: return False
 
 if __name__ == "__main__":
     password = input("Please enter a password: ")
