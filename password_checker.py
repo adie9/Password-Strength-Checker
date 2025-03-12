87ae2b61a399a414efe0124f9fb8238d7069f18d
Adrian Guerra <70285960+adie9@users.noreply.github.com>
2025-03-12 15:59:21 -0400

    Added numeric and alphabetical checkers

--- password_checker.py
+++ password_checker.py
@@ -1,9 +1,29 @@
 # Main password strength checker function
 
 def password_checker(password):
-    
-    # Does the password contain letters?
-    print(f"Your new password is: {password}")
+    length = False
+    letters = False
+    numbers = False
+
+    # Is the password longer than eight characters?
+    if len(password) > 8:
+        length = True
+    else: False
+
+
+    # Does the password have numbers?
+    numbers = any(char.isdigit() for char in password)
+        
+
+    # Does the password have letters?
+    letters = any(char.isalpha() for char in password)
+            
+            
+    print(numbers)
+    print(letters)
+
+    # Print the password
+    #print(f"Your new password is: {password}")
 
 
 if __name__ == "__main__":
